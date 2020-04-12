d7dc093a7529108247ecd6769fd0750d9024ae06
Splintert <splintert@ymail.com>
2020-04-11 19:07:36 -0500

    Add project files.

---
+++ PersistentLock/PersistentLockMod.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem.ViewModelCollection;
+
+namespace PersistentLock
+{
+    public class Globals
+    {
+        public static bool firstLoad = true;
+        public static HashSet<string> lockedItems = new HashSet<string>();
+    }
+
+    [HarmonyPatch(typeof(SPInventoryVM), "SaveItemLockStates")]
+    public class SaveItemLockStatesMod
+    {        
+        static void Postfix(SPInventoryVM __instance)
+        {
+            foreach (SPItemVM item in (__instance as SPInventoryVM).RightItemListVM)
+            {
+                if (item.IsLocked || (Globals.lockedItems.Contains(item.StringId)))
+                    Globals.lockedItems.Add(item.StringId);
+                else
+                    Globals.lockedItems.Remove(item.StringId);
+            }
+
+            Globals.firstLoad = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(SPInventoryVM), "InitializeInventory")]
+    public class InitializeInventoryMod
+    {
+        static void Postfix(SPInventoryVM __instance)
+        {
+            if (Globals.firstLoad)
+                return;
+
+            foreach (SPItemVM item in (__instance as SPInventoryVM).RightItemListVM)
+                item.IsLocked = Globals.lockedItems.Contains(item.StringId);
+        }
+    }
+}
---
+++ PersistentLock/SubModule.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System;
+using System.Windows.Forms;
+using TaleWorlds.MountAndBlade;
+
+namespace PersistentLock
+{
+    class SubModule : MBSubModuleBase
+    {
+        public static readonly string ModuleFolderName = "PersistentLock";
+
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            try
+            {
+                var harmony = new Harmony("mod.bannerlord.splintert");
+                harmony.PatchAll();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error Initialising PersistentLock:\n\n{ex.Message}");
+            }
+        }
+    }
+}
